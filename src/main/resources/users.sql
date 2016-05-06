e1816c0df59d0dbe530fe3ea1d32f91369373dd8
JimmyZhang <jimmyzhang144@gmail.com>
2016-05-06 11:30:32 +0800

    提交数据库操作表

---
+++ src/main/resources/users.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : icolleague
+Source Server Version : 50534
+Source Host           : localhost:3306
+Source Database       : mysql
+
+Target Server Type    : MYSQL
+Target Server Version : 50534
+File Encoding         : 65001
+
+Date: 2016-05-06 11:27:34
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) DEFAULT NULL,
+  `email` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('1', 'JimmyZhang', '277936294@qq.com');
